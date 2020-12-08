4e94142143092eb6ba346b0f008a654da30ff265
swaatipandey <swati.pandey@contentstack.com>
2020-12-08 21:45:29 +0530

    made navbar responsive

--- src/components/sideContent.js
+++ src/components/sideContent.js
@@ -12,21 +12,3 @@ export const sideContent = (links) => {
   });
   return ul;
 };
-//     li.addEventListener("click",()=>{
-//         renderNewBlog(li.id)
-//     }) = renderNewBlog;
-//     ul.appendChild(li);
-//   });
-
-//     div.classList.add("side-content");
-//     const ul = document.createElement(ul);
-//     // const arr = [links];
-//     links.forEach(e,i){
-//         document.getElementById("side-content").innerHTML
-
-//     }
-//     function myfunt()
-//     // ul.innerHtml = links;
-//     // div.appendChild(ul);
-//     // return div;
-// }
